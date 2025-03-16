83a69ea35451fed9d0c8b40e9bd904df0ea1b5f5
ldngduong <letungduong1624@gmail.com>
2025-03-17 02:59:16 +0700

    upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
 import Navbar from "./Navbar";
 import Information from "./Information";
 import Objective from "./Objective";
 import Skill from "./Skill";
 import Project from "./Project";
+import Loading from "./Loading"; // Import component loading của bạn
 
 const Home = () => {
-  
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // Chờ 1 giây trước khi ẩn loading
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="bg-gray-50">
-    <Navbar />
-    <ReactFullpage
-      scrollingSpeed={1000}
-      render={() => (
-        <ReactFullpage.Wrapper>
-          <div className="section flex flex-col h-screen">
-            <div className="flex-grow p-5 relative">
-              <Information />
+      <Navbar />
+      <ReactFullpage
+        scrollingSpeed={1000}
+        render={() => (
+          <ReactFullpage.Wrapper>
+            <div className="section flex flex-col h-screen">
+              <div className="flex-grow p-5 relative">
+                <Information />
+              </div>
             </div>
-          </div>
-          <div className="section flex flex-col h-screen">
-            <div className="flex-grow p-5 relative flex flex-col">
-              <Objective />
+            <div className="section flex flex-col h-screen">
+              <div className="flex-grow p-5 relative flex flex-col">
+                <Objective />
+              </div>
             </div>
-          </div>
-          <div className="section flex flex-col h-screen">
-            <div className="flex-grow p-5 relative flex flex-col">
-              <Skill />
+            <div className="section flex flex-col h-screen">
+              <div className="flex-grow p-5 relative flex flex-col">
+                <Skill />
+              </div>
             </div>
-          </div>
-          <div className="section flex flex-col h-screen">
-            <div className="flex-grow p-5 relative flex flex-col ">
-              <Project />
+            <div className="section flex flex-col h-screen">
+              <div className="flex-grow p-5 relative flex flex-col ">
+                <Project />
+              </div>
             </div>
-          </div>
-        </ReactFullpage.Wrapper>
-      )}
-    />
+          </ReactFullpage.Wrapper>
+        )}
+      />
     </div>
   );
 };
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div class="flex justify-center items-center h-screen">
+        <div class="rounded-full h-20 w-20 bg-gray-600 animate-ping"></div>
+    </div>
+  )
+}
+
+export default Loading
